afcc85947f1c42955833689b75a5bdc8de25fe50
Mariano Jara <mjaravillasana@gmail.com>
2025-10-16 00:36:44 -0600

    Me faltaba el permiso del internet loooool, no tocaré el tema nunca más, media hora revisando el código lol

--- app/src/main/java/com/example/musicapp/Screens/AlbumDetail.kt
+++ app/src/main/java/com/example/musicapp/Screens/AlbumDetail.kt
@@ -1,9 +1,50 @@
 package com.example.musicapp.Screens
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.musicapp.Models.Album
+import com.example.musicapp.Services.AlbumService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Composable
 fun AlbumDetail(id: String ){
+
+    var album by remember {
+        mutableStateOf<Album?>(null)
+
+    }
+
+    LaunchedEffect(true) {
+        try {
+            val retrofit = Retrofit
+                .Builder()
+                .baseUrl("https://music.juanfrausto.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            val service = retrofit.create(AlbumService::class.java)
+            val result = withContext(Dispatchers.IO) {
+                service.getAlbumId(id)
+            }
+
+            album = result
+            Log.i("ProductDetailScreen", album.toString())
+
+        } catch (e: Exception) {
+            Log.e("ProductDetailScreen", e.toString())
+        }
+    }
+    Column {
+        Text(text = "${album?.id}")
+    }
 
 }
